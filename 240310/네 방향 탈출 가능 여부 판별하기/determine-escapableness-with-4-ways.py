a037676daf166da1a4e92bb2a797947f91bec678
Woo SeungHun <woosh98@gmail.com>
2024-03-10 18:17:48 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 147ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,33 @@
+from sys import stdin, setrecursionlimit
+from collections import deque
+input = stdin.readline
+
+setrecursionlimit(10**4)
+N, M = map(int, input().split())
+
+board = []
+for i in range(N):
+    board.append(list(map(int, input().split())))
+
+visited = [[False] * M for _ in range(N)]
+
+#(y, x)
+directions = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+dq = deque()
+def bfs():
+    visited[0][0] = True
+    while dq:
+        y, x = dq.popleft()
+        for dy, dx in directions:
+            nx = x + dx
+            ny = y + dy
+
+            if 0 <= nx < M and 0 <= ny < N:
+                if not visited[ny][nx] and board[ny][nx]:
+                    visited[ny][nx] = 1
+                    dq.append((ny, nx))
+
+
+dq.append((0, 0))
+bfs()
+print(visited[N-1][M-1])
