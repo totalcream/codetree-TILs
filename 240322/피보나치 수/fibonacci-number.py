8c43f242b6a6a8bbe4be12f9d879f2be35e4d5bc
Woo SeungHun <woosh98@gmail.com>
2024-03-22 02:32:45 +0900

    Title: 피보나치 수, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/피보나치 수/fibonacci-number.py
+++ 240322/피보나치 수/fibonacci-number.py
@@ -5,10 +5,13 @@ N = int(input())
 
 dp = [0 for i in range(N + 1)]
 
-dp[1] = 1
-dp[2] = 1
+if N < 3:
+    print(1)
+else:
+    dp[1] = 1
+    dp[2] = 1
 
-for i in range(3, N + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
 
-print(dp[N])
+    print(dp[N])
