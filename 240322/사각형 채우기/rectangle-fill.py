b6d9717ba0b107a9ebd94cb891fd14671eea9727
Woo SeungHun <woosh98@gmail.com>
2024-03-22 15:42:39 +0900

    Title: 사각형 채우기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/사각형 채우기/rectangle-fill.py
+++ 240322/사각형 채우기/rectangle-fill.py
@@ -1,7 +1,7 @@
 N = int(input())
 
 dp = [0] * (N + 1)
-
+MOD = 10007
 if N == 1:
     print(1)
 elif N == 2:
@@ -10,6 +10,6 @@ else:
     dp[1] = 1
     dp[2] = 2
     for i in range(3, N + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
 
 print(dp[N])
